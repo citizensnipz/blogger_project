e42e8e02efd0d4f26182decb8d81193c46702583
Josh Kroslowitz <joshkroz@gmail.com>
2016-10-21 02:56:21 -0400

    added update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,4 +18,13 @@ class ArticlesController < ApplicationController
 		Article.find(params[:id]).destroy
 		redirect_to action: 'index'
 	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		redirect_to article_path(@article)
+	end
 end
